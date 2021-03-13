929ff89091f9886560c8b9c9a19f65bb2e5ef25e
Ali Bas <alibas01@gmail.com>
2021-03-13 09:57:23 -0500

    latest

--- backend/app/controllers/remote_controller.rb
+++ backend/app/controllers/remote_controller.rb
@@ -2,14 +2,14 @@ class RemoteController < ApplicationController
   
   def index
   end
-  
-  include HTTParty
-  base_uri 'https://aviation-edge.com/v2/public/'
+
+  # include HTTParty
+  # base_uri 'https://aviation-edge.com/v2/public/'
 
   def flight_post
     code = Flight.flight_number
-    self.class.get('flights?key=e58eb2-cea570&flightIata = #{code}')
-    #puts Flight.flight_post
-  end 
+    flight_info = HTTParty.get('https://aviation-edge.com/v2/public/flights?key=e58eb2-cea570&flightIata = #{code}')
+  end
+
 
 end
