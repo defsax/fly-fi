48af4620ec7009a1f5f5ea50d2717c7d02f36f44
Perry Defayette <defayetteperry@gmail.com>
2021-03-13 17:41:09 +0000

    Create users model and controller.

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/create'
+  get 'users/update'
+  get 'users/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
