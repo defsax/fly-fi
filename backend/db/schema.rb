7f673b343ecb93913fd71a72f61b8c1e359bc949
Per <defayetteperry@gmail.com>
2021-03-14 18:22:47 -0400

    Add migration to change phone from int to string.

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_172752) do
+ActiveRecord::Schema.define(version: 2021_03_14_221857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_03_13_172752) do
     t.string "name"
     t.string "email"
     t.string "password"
-    t.integer "phone"
+    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
