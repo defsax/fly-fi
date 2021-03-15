6478ebf9c86a83b80aab917e77c183e9415e8438
Per <defayetteperry@gmail.com>
2021-03-15 17:48:09 -0400

    Change app page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Switch, 
+  Route 
+} from 'react-router-dom';
+
 import Welcome from "./components/Welcome";
 
 export default function App(){
