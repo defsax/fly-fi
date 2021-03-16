0239992e0f495895924b5fe4c91aa98014bb8fdc
Mike Ackison <mike.ackison@shopify.com>
2021-03-16 13:39:34 -0400

    moved submit form to index.jsx, created set mode and useVsual mode, results page mounting after search

--- frontend/src/components/home/sidePanel/Loading.jsx
+++ frontend/src/components/home/sidePanel/Loading.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-export default function Loading() {
+export default function Loading(props) {
   return(
-    <h1>I am Loading component</h1>
+    <main className="appointment__card appointment__card--status">
+      <h1>I am Loading component</h1>
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">{props.message}</h1>
+    </main>
   )
 }
--- frontend/src/components/home/sidePanel/index.jsx
+++ frontend/src/components/home/sidePanel/index.jsx
@@ -6,6 +6,14 @@ import Register from "./Register"
 import Results from "./Results"
 import Search from "./Search"
 import useAPIData from "../../../hooks/useAPIData"
+import useVisualMode from '../../../hooks/useVisualMode'
+import axios from "axios"
+
+
+const ERROR = 'ERROR';
+const LOADING = "LOADING"
+const SEARCH = "SEARCH"
+const SHOW = "SHOW"
 
 export default function SidePanel(props) {
 
@@ -18,23 +26,61 @@ export default function SidePanel(props) {
     setResults
   } = useAPIData();
 
+  const {mode, transition, back } = useVisualMode(
+    props.flightInfo ?  SHOW : SEARCH
+  )
 
+  const submitSearchForm = function () {
+    transition(LOADING)
+    axios.post('/search', {flight: {flight_number: flightInfo.flightNumber, dep_airport: flightInfo.departureAirport, arr_airport: flightInfo.arrivalAirport}})
+    .then(response => {
+      transition(SHOW, true)
+      if(response.data.error) {
+        console.log(response.data.error)
+      }
+      else {
+        console.log('response', response.data);
+        setResults([...response.data]);
+        // reset();
+      }
+    })
+  }
   
   return(
     <div>
+      <article className='sidePanel'>
+
       <h1>This is sidePanel</h1>
-      <Error />
-      <Loading />
-      <Login />
-      <Register />
+
+
+      {mode === ERROR && (
+        <Error 
+        message="There was an Error"
+        onClose={back}
+        />
+      )}
+     {mode === LOADING &&
+       <Loading message="Loading" />
+      }
+
+
+      {/* <Login />
+      <Register /> */}
+      {mode === SEARCH && (
       <Search 
-        setResults={setResults} 
+      submitSearchForm={submitSearchForm}
+        // setResults={setResults} 
         flightInfo={flightInfo}
         setFlightInfo={setFlightInfo}
         notification={notification}
         setNotification={setNotification}
         />
+      )}
+
+      {mode === SHOW && (
       <Results flightList={results} />
+      )}
+      </article>
     </div>
   )
 }
