1dba4c78b357fb4d5c5260a6c0f0674f46a68045
Per <defayetteperry@gmail.com>
2021-03-16 16:33:46 -0400

    Add welcome stylesheet

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import welcome from "../styles/scss/welcome.scss"
 
 import Button from "./Button";
 
 export default function Welcome(props) {  
   return(
-    <div>
+    <div className="center">
       <h1>I am welcome component</h1>
       <Link to='/home'>
         <Button text="Home" disabled={false} />
