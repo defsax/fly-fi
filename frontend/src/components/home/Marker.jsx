e2c724fdcbcce067f53648cccc251304e20da573
Mike Ackison <mike.ackison@shopify.com>
2021-03-18 17:10:54 -0400

    add id to marker to use font awesome plane icon

--- frontend/src/components/home/Marker.jsx
+++ frontend/src/components/home/Marker.jsx
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
-import "../../styles/scss/marker.scss";
+import '../../styles/scss/marker.scss';
 
 const config = {
   markerLongitude: 45.424721,
-  markerLatitude: -75.695000,
-  googleMapsMarkerIcon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINp6ut9j1blvcfrkgxQpYDXfnW_LhGhOXcg&usqp=CAU'
-}
+  markerLatitude: -75.695,
+  googleMapsMarkerIcon:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINp6ut9j1blvcfrkgxQpYDXfnW_LhGhOXcg&usqp=CAU',
+};
 
 export default function Marker(props) {
-
-  
   return (
-    <img 
-    className='marker' 
-    src={config.googleMapsMarkerIcon}
-    onClick={props.onClick} 
-    />
-    // <i className="fas fa-plane"></i>
-  )
+    // <img
+    // className='marker'
+    // src={config.googleMapsMarkerIcon}
+    // onClick={props.onClick}
+    // />
+    <i id='marker' className='fas fa-plane'></i>
+  );
 }
