e8910671ec836cd3a110e755840826034630665a
Per <defayetteperry@gmail.com>
2021-03-18 19:06:41 -0400

    Modify search controller for new api call.

--- backend/app/controllers/search_controller.rb
+++ backend/app/controllers/search_controller.rb
@@ -2,9 +2,11 @@ class SearchController < ApplicationController
   def search 
     # change query based on given params
     if flight_param[:flight_number].length != 0
-      url = "https://aviation-edge.com/v2/public/flights?key=#{ENV['AVIATION_API_KEY']}&flightIata=#{flight_param[:flight_number]}"
-    else
+      url = "https://aviation-edge.com/v2/public/flights?key=#{ENV['AVIATION_API_KEY']}&flightIata=#{flight_param[:flight_number]}"      
+    elsif flight_param[:arr_airport].length != 0 && flight_param[:arr_airport].length != 0
       url = "https://aviation-edge.com/v2/public/flights?key=#{ENV['AVIATION_API_KEY']}&depIata=#{flight_param[:dep_airport]}&arrIata=#{flight_param[:arr_airport]}"
+    else
+      url = "https://aviation-edge.com/v2/public/flights?key=#{ENV['AVIATION_API_KEY']}&lat=#{flight_param[:coords.lat]}&lng=#{flight_param[:coords.lng]}&distance=#{flight_param[:distance]}"
     end
 
     response = HTTParty.get(url)
@@ -14,6 +16,6 @@ class SearchController < ApplicationController
   private
 
   def flight_param
-    params.require(:flight).permit(:flight_number, :dep_airport, :arr_airport)
+    params.require(:flight).permit(:flight_number, :dep_airport, :arr_airport, :distance, :coords)
   end
 end
