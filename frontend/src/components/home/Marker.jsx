9c97f4f6b0ab7bcc0c8a02fb1965d51228f8235b
Mike Ackison <mike.ackison@shopify.com>
2021-03-19 10:21:15 -0400

    put logic in a function for plane direction

--- frontend/src/components/home/Marker.jsx
+++ frontend/src/components/home/Marker.jsx
@@ -10,18 +10,18 @@ const config = {
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRINp6ut9j1blvcfrkgxQpYDXfnW_LhGhOXcg&usqp=CAU',
 };
 
+const renderMarkerDIrection = (props) => {
+  const flightDirection = props - 90;
+  return { transform: 'rotate(' + flightDirection + 'deg)' };
+};
+
 export default function Marker(props) {
-  let flightDirection = props.direction - 90;
+  // let flightDirection = props.direction - 90;
   return (
-    // <img
-    // className='marker'
-    // src={config.googleMapsMarkerIcon}
-    // onClick={props.onClick}
-    // />
     <i
       id='marker'
       className='fas fa-plane'
-      style={{ transform: 'rotate(' + flightDirection + 'deg)' }}
+      style={renderMarkerDIrection(props.direction)}
       onClick={props.onClick}
     ></i>
   );
