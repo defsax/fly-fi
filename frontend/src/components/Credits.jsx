3031375ff4957e901b71a63362b845dc28944cfe
Ali Bas <alibas01@gmail.com>
2021-03-20 21:59:12 -0400

    credits page images one addition

--- frontend/src/components/Credits.jsx
+++ frontend/src/components/Credits.jsx
@@ -4,9 +4,23 @@ export default function Credits(props) {
   console.log('here')
   return (
     <div className="credits">
+      <p>Fly-Fi is a single page application built with multiple frameworks as it is shown on the page. <br />LightHouse Labs Final Project</p>
       <section className="contributors">
-        <img src="../../public/images/Perry.png" />
+        <h1>CREDITS:</h1>
+        <div>
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />
+          <h1>Perry Defayette</h1>
+        </div>
+        <div>
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />
+          <h1>Ali Bas</h1>
+        </div>
+        <div>
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />
+          <h1>Mike Ackison</h1>
+        </div>
       </section>
+
     </div>
   );
 }
