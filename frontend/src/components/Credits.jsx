0322fb448a082a279664208b2e3660e263080a3c
Ali Bas <alibas01@gmail.com>
2021-03-20 22:22:08 -0400

    new nav-bar without login functions

--- frontend/src/components/Credits.jsx
+++ frontend/src/components/Credits.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export default function Credits(props) {
-  console.log('here')
   return (
     <div className="credits">
+      <div className="nav">
+        <nav className="navBarItems" isloggedin={props.isloggedin}>  
+          <Link to='/'>
+            <img className="navbar-logo" alt="Fly-Fi" src={"https://raw.githubusercontent.com/defsax/fly-fi/master/frontend/public/images/fly-fi-logo.png"}></img>
+          </Link>
+          <Link to='/credits'>
+          <ul className="nav-menu">
+            <li className="nav-item">
+              <button className="nav-button">
+                Credits
+              </button>
+            </li>
+          </ul>
+          </Link>
+        </nav>
+      </div>
       <p>Fly-Fi is a single page application built with multiple frameworks as it is shown on the page. <br />LightHouse Labs Final Project</p>
       <section className="contributors">
         <h1>CREDITS:</h1>
@@ -19,6 +35,22 @@ export default function Credits(props) {
           <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />
           <h1>Mike Ackison</h1>
         </div>
+      </section>
+      <section className="frontend">
+        <h1>FRONTEND:</h1>
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} /> 
+        
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} /> 
+        
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />  
+      </section>
+      <section className="backend">
+        <h1>BACKEND:</h1>
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} /> 
+        
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} /> 
+        
+          <img src={"https://github.com/defsax/fly-fi/blob/master/frontend/public/images/fly-fi-logo.png"} />  
       </section>
 
     </div>
